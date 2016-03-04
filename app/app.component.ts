4efc1cd2549c082c17a3e5b0c2e29336fa90fa20
Marc Ludwig <marc.ludwig@sap.com>
2016-03-04 15:06:13 +0000

    Add dashboard route, all working.

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,7 +8,10 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/route
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <a [routerLink]="['Heroes']">Heroes</a>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES],
