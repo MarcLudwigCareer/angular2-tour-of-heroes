45e48ad2fb36a28128f0ccb17ba7ed177193c6af
Marc Ludwig <marc.ludwig@sap.com>
2016-03-04 15:02:59 +0000

    Add new route, not currently used.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component }       from 'angular2/core';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
 @Component({
@@ -17,6 +18,12 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/route
   ]
 })
 @RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
   {
     path: '/heroes',
     name: 'Heroes',
